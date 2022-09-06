c686a644bc8d1bd6e8fc28834eae74ea2c87356e
Chien Tran <chientrm@gmail.com>
2022-09-06 11:13:48 +0000

    fix: podie

--- podie.sh
+++ podie.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-apt-get install\
+apt-get update && apt-get upgrade -y
+
+apt-get install -y \
   nodejs\
   npm\
   liburing-dev\
   build-essential\
   python3\
+
+apt-get autoremove -f -y
